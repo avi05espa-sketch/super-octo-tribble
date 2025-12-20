28ddcb5cc0ecc31a84f6d98b7e9a46a245653330
Avi Espa <avi05espa@gmail.com>
2025-12-20 10:07:42 +0000

    Hazlo cosa por cosa una a la vez

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,15 +1,16 @@
+
 // This file is not intended to be edited.
 // It is used to deploy and configure Firebase services.
 // To configure your Firebase project, edit the service-account.json file.
 import { FirebaseOptions, getApp, getApps, initializeApp } from 'firebase/app';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "REDACTED",
+  authDomain: "tijuana-shop.firebaseapp.com",
+  projectId: "tijuana-shop",
+  storageBucket: "tijuana-shop.appspot.com",
+  messagingSenderId: "923422739335",
+  appId: "1:923422739335:web:2a823c316c8f58b7a6953f"
 };
 
 
